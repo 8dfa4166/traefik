e1b11351979304eafcebd8a437a057fa890bb362
8dfa4166 <8dfa4166@gmail.com>
2024-06-07 18:29:49 +0900

    added npm run dev script

--- web/index.ts
+++ web/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
+import { ExpressAuth } from "@auth/express";
+import Keycloak from "@auth/express/providers/keycloak";
 
 const app = express();
 const port = 3000;
 
+app.set("trust proxy", true);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+// app.use("/auth/*", ExpressAuth({ providers: [Keycloak] }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

9641d6cda13223a05d529f3f15f83c8c83edeea4
8dfa4166 <8dfa4166@gmail.com>
2024-06-07 18:42:14 +0900

    added type=module

--- web/index.ts
+++ web/index.ts
@@ -9,7 +9,8 @@ app.set("trust proxy", true);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
-// app.use("/auth/*", ExpressAuth({ providers: [Keycloak] }));
+
+app.use("/auth/*", ExpressAuth({ providers: [Keycloak] }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
